1ab0fc68ddb0399d0d275eeeca6c595492a298cd
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-10 22:36:08 +0200

    Added rate reader

---
+++ Exchange/Exchange/RateReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exchange
+{
+    public class RateReader
+    {
+        public string FileRatePath { get; set; }
+
+        public Dictionary<string, double> CurrencyRates { get; private set; }
+
+        private Dictionary<string, double> HardcodedCurrencyRates => new Dictionary<string, double>
+        {
+            {"EUR", 743.94 },
+            {"USD", 663.11 },
+            {"GBP", 852.85 },
+            {"SEK", 76.10 },
+            {"NOK", 78.40 },
+            {"CHF", 683.58 },
+            {"JPY", 5.9740 }
+        } ;
+
+        public RateReader()
+        {
+            if (FileRatePath == string.Empty)
+            {
+                CurrencyRates = HardcodedCurrencyRates;
+            }
+        }
+    }
+}
