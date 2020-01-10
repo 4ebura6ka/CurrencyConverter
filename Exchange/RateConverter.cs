405a38a06065b4abb10dbefb61fecff11e21f555
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-10 23:11:04 +0200

    Added rate converter

---
+++ Exchange/RateConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exchange
+{
+    public class RateConverter
+    {
+        private Dictionary<string, double> currencyRates;
+
+        public RateConverter(Dictionary<string, double> currencyRates)
+        {
+            this.currencyRates = currencyRates;
+        }
+
+        public double Convert(string mainCurrency, string moneyCurrency, double amountOfMoney)
+        {
+            if (!currencyRates.ContainsKey(mainCurrency) || !currencyRates.ContainsKey(moneyCurrency))
+            {
+                throw new ArgumentException("Specified currencies not found in provided rates");
+            }
+
+            double rateForSpecifiedCurrencies = currencyRates[mainCurrency] / currencyRates[moneyCurrency];
+
+            return rateForSpecifiedCurrencies * amountOfMoney;
+        }
+    }
+}
