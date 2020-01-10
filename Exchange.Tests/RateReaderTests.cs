cb37328c095c90f7ad2c9f5a0fb5fc14d25b9d61
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-10 23:10:38 +0200

    Renamed test file

---
+++ Exchange.Tests/RateReaderTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Exchange;
+
+namespace Exchange.Tests
+{
+    public class RateReaderTests
+    {
+        [Test]
+        public void RateReaderHardcodedCurrencyRatesResult()
+        {
+            RateReader rateReader = new RateReader();
+
+            Assert.IsTrue(rateReader.CurrencyRates["NOK"] == 78.40);
+        }
+    }
+}
