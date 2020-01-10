865610290cb8bcf57a34c0172ef486946bfc993e
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-10 23:58:45 +0200

    Added argument reading

--- Exchange/Program.cs
+++ Exchange/Program.cs
@@ -3,10 +3,25 @@
 namespace Exchange
 {
     class MainClass
-    {
+    { 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Usage: Exchange <currency pair> <amount to exchange>");
+            }
+
+            RateReader rateReader = new RateReader();
+
+            string currencyRate = args[0];
+            double moneyAmount = double.Parse(args[1]);
+
+            string mainCurrency = currencyRate.Substring(0, currencyRate.IndexOf("/"));
+            string moneyCurrency = currencyRate.Substring(currencyRate.IndexOf("/"), currencyRate.Length);
+
+            RateConverter rateConverter = new RateConverter(rateReader.CurrencyRates);
+
+            rateConverter.Convert(mainCurrency, moneyCurrency, moneyAmount);
         }
     }
 }
