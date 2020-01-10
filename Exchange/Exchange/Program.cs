d75b6f132a3da12cd18f2303810c7bc37315f4c2
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-10 20:59:24 +0200

    Solution created

---
+++ Exchange/Exchange/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exchange
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
