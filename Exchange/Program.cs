2d4dcf4b40580de1c0ad9ddfb5af8293526f3612
Sergej Poltorak <sergej.poltorak@gmail.com>
2020-01-14 00:19:54 +0200

    Removed hardcoded path to rates file, added trivial arguments checks

--- Exchange/Program.cs
+++ Exchange/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.IO;
 
 namespace Exchange
 {
     class MainClass
-    { 
+    {
         public static void Main(string[] args)
         {
             if (args.Length <= 0)
@@ -11,20 +12,31 @@ namespace Exchange
                 Logging.Log("Usage: Exchange <currency pair> <amount to exchange>");
             }
 
-            double moneyAmount = double.Parse(args[1]);
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string ratesFilePath = Directory.GetParent(currentDirectory).Parent.ToString() + "/sample1.txt";
+            RateReader rateReader = new RateReader(ratesFilePath);
 
+            double moneyAmount;
+            if (!double.TryParse(args[1], out moneyAmount))
+            {
+                throw new ArgumentException("Amount to exchange is invalid");
+            }
+           
             string currencyCombination = args[0];
+            if (currencyCombination.Length < 7)
+            {
+                throw new ArgumentException("Currency pair is invalid");
+            }
+
             string[] currencies = currencyCombination.Split('/');
             string mainCurrency = currencies[0];
             string moneyCurrency = currencies[1];
-
-            RateReader rateReader = new RateReader("/Users/serz/Projects/Exchange/Exchange/sample1.txt");
 
             RateConverter rateConverter = new RateConverter(rateReader.CurrencyRates);
 
             double result = rateConverter.Convert(mainCurrency, moneyCurrency, moneyAmount);
 
-            Logging.Log (result.ToString());
+            Logging.Log(result.ToString());
         }
     }
 }
